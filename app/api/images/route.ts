a8c8742b72425bab54232e47c181168692bafada
Preetam <preetampatil2324@gmail.com>
2025-01-16 17:04:51 +0530

    Image to text feature

---
+++ app/api/images/route.ts
@@ -0,0 +1,23 @@
+import { analyzeImage } from "@/utils/imageUtils";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  if (!req.headers.get("content-type")?.includes("multipart/form-data")) {
+    return NextResponse.json(
+      { error: "Content type must be multipart/form-data" },
+      { status: 400 }
+    );
+  }
+  const formData = await req.formData();
+  const image = formData.get("image") as File;
+
+  //   console.log(image);
+  const data = await analyzeImage(image);
+
+  // console.log(data);
+
+  return NextResponse.json({
+    message: "image parsed",
+    data,
+  });
+}
---
+++ utils/imageUtils.ts
@@ -0,0 +1,85 @@
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { Jimp } from "jimp";
+import Tesseract from "tesseract.js";
+
+import { generateObject, generateText } from "ai";
+import { z } from "zod";
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GOOGLE_API_KEY,
+});
+
+const openAI = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function analyzeImage(file: File, textRatioThreshold = 0.0003) {
+  const buffer = Buffer.from(await file.arrayBuffer());
+  const image = await Jimp.read(buffer);
+  const imageArea = image.bitmap.width * image.bitmap.height;
+
+  const recognizedLanguage = "eng+hin";
+
+  const {
+    data: { text },
+  } = await Tesseract.recognize(buffer, recognizedLanguage, {
+    cacheMethod: "none",
+  });
+
+  // console.log(text);
+
+  //   Calculate text density
+  const textLength = text.trim().length;
+  const textDensity = textLength / imageArea;
+  // console.log(textDensity);
+
+  if (textDensity > textRatioThreshold) {
+    const response2 = await generateObject({
+      model: google("gemini-1.5-flash-latest"),
+      schema: z.string(),
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: "For the given text please fix the punctuation error if there are any and return it",
+            },
+            {
+              type: "text",
+              text: `${text}`,
+            },
+          ],
+        },
+      ],
+    });
+
+    return response2.object;
+  } else {
+    const base64Image = buffer.toString("base64");
+    const response = await generateText({
+      model: google("gemini-1.5-flash-latest"),
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: "Please summarize the given image in 5-6 lines",
+            },
+            {
+              type: "image",
+              image: `data:image/png;base64,${base64Image}`,
+            },
+          ],
+        },
+      ],
+    });
+
+    // console.log(response.text);
+    return response.text;
+  }
+
+  // return textDensity > textRatioThreshold;
+}
